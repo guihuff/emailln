e0ca56c2ed90e2bf22b56d4809f0c33c1e5472de
Guilherme Feier Huff <ghuffg00@gmail.com>
2023-07-26 12:09:20 -0300

    campaing post test

--- internal/endpoints/campaigns_post_test.go
+++ internal/endpoints/campaigns_post_test.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	"bytes"
+	"context"
 	"emailln/internal/contract"
 	internalmock "emailln/internal/test/internalMock"
 	"encoding/json"
@@ -16,18 +17,32 @@ import (
 
 var (
 	body = contract.NewCampaign{
-		Name:    "teste",
-		Content: "Hi everyone",
-		Emails:  []string{"teste@teste.com"},
+		Name:      "teste",
+		Content:   "Hi everyone",
+		Emails:    []string{"teste@teste.com"},
+		CreatedBy: "teste@create.com",
 	}
 )
+
+func setup() (*http.Request, *httptest.ResponseRecorder) {
+	var buf bytes.Buffer
+	json.NewEncoder(&buf).Encode(body)
+	req, _ := http.NewRequest("POST", "/", &buf)
+	ctx := context.WithValue(req.Context(), "email", body.CreatedBy)
+	req = req.WithContext(ctx)
+	res := httptest.NewRecorder()
+
+	return req, res
+}
 
 func Test_CampaignPost_should_save_new_campaign(t *testing.T) {
 	assert := assert.New(t)
 
 	service := new(internalmock.CampaignServiceMock)
 	service.On("Create", mock.MatchedBy(func(request contract.NewCampaign) bool {
-		if request.Name == body.Name && request.Content == body.Content {
+		if request.Name == body.Name &&
+			request.Content == body.Content &&
+			request.CreatedBy == body.CreatedBy {
 			return true
 		} else {
 			return false
@@ -35,10 +50,7 @@ func Test_CampaignPost_should_save_new_campaign(t *testing.T) {
 	})).Return("34x", nil)
 	handler := Handler{CampaignService: service}
 
-	var buf bytes.Buffer
-	json.NewEncoder(&buf).Encode(body)
-	req, _ := http.NewRequest("POST", "/", &buf)
-	res := httptest.NewRecorder()
+	req, res := setup()
 
 	_, status, err := handler.CampaignPost(res, req)
 
@@ -53,10 +65,7 @@ func Test_CampaignPost_should_inform_error_when_exist(t *testing.T) {
 	service.On("Create", mock.Anything).Return("", fmt.Errorf("error"))
 	handler := Handler{CampaignService: service}
 
-	var buf bytes.Buffer
-	json.NewEncoder(&buf).Encode(body)
-	req, _ := http.NewRequest("POST", "/", &buf)
-	res := httptest.NewRecorder()
+	req, res := setup()
 
 	_, _, err := handler.CampaignPost(res, req)
 
